5c22bc5197b5abe797fda818f13e92cd445585d0
mRasey <billywzh@126.com>
2016-08-18 15:59:34 +0800

    workplace

--- Hello/Hello.js
+++ Hello/Hello.js
@@ -1,12 +1,11 @@
-/**
- * Created by Billy on 2016/8/11.
- */
-function main() {
-    var a = 1;
-    var a = 2;
-    var a = {
-        a: 1,
-        a: 2
-    }
-    var a = a.a + a.a
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    
+    Person.prototype.toString = function () {
+        return this.name + " " + this.age;
+    };
 }
+
+var person1 = new Person("person1", 17);
+document.write(person1.toString());
